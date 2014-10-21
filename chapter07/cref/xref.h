e8ccfbe7774aace391c1c9d3d76437c8df08a371
lanbeilyj <975468079@qq.com>
2014-10-21 22:49:07 +0800

    renew

--- chapter07/cref/xref.h
+++
@@ -1,8 +0,0 @@
-#ifndef xref_H
-#define xref_H
-#include <vector>
-#include <map>
-#include <string>
-std::map<std::string,std::vector<int> >xref(std::istream& in,std::vector<std::string> find_words(const std::string& s));
-
-#endif // xref_H
