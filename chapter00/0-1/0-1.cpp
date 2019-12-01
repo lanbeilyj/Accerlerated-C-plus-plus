0fe2c66b022079a1b4556be428eb1b342b1e7131
Python-Zhou <40736562+Python-Zhou@users.noreply.github.com>
2019-12-01 15:01:21 +0800

    Update 0-1.cpp
    There are some wrong symbols.

--- chapter00/0-1/0-1.cpp
+++ chapter00/0-1/0-1.cpp
@@ -3,6 +3,8 @@
 *3 + 4;
 */
 
-3+4 just a expression,the result is 7£¬no side effects¡£
+3+4 just a expression,the result is 7, no side effects;
 
-tips£ºsome expression may be have side effects on the program or the system environment£¬such as exercises 0-0£¬cout<<"Hello,world!"<<endl;the value of the expression is: cout£¬but in the mean time the string:¡°Hello,world£¡"also output to the standard ostream¡£
+tips: some expression may be have side effects on the program or the system environment,
+such as exercises 0-0, cout<<"Hello,world!"<<endl; the value of the expression is: cout,
+but in the mean time the string:"Hello,world!"also output to the standard ostream.
